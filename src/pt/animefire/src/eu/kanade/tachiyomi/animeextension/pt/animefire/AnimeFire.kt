0ce14cf0ae674fb6e2065a582d0bd4a806373895
Bashousen <109592830+Bashousen@users.noreply.github.com>
2026-06-05 15:26:17 -0300

    [pt/animesfire] Change the video extractor. - Change IframeExtractor to blogger-extractor

--- src/pt/animefire/src/eu/kanade/tachiyomi/animeextension/pt/animefire/AnimeFire.kt
+++ src/pt/animefire/src/eu/kanade/tachiyomi/animeextension/pt/animefire/AnimeFire.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animeextension.pt.animefire.extractors.AnimeFireExtractor
-import eu.kanade.tachiyomi.animeextension.pt.animefire.extractors.IframeExtractor
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
@@ -12,6 +11,7 @@ import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.lib.bloggerextractor.BloggerExtractor
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.awaitSuccess
 import eu.kanade.tachiyomi.util.asJsoup
@@ -146,13 +146,16 @@ class AnimeFire : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     // ============================ Video Links =============================
+    val bloggerExtractor by lazy { BloggerExtractor(client) }
+
     override fun videoListParse(response: Response): List<Video> {
         val document = response.asJsoup()
         val videoElement = document.selectFirst("video#my-video")
         return if (videoElement != null) {
             AnimeFireExtractor(client, json).videoListFromElement(videoElement, headers)
         } else {
-            IframeExtractor(client).videoListFromDocument(document, headers)
+            val url = document.selectFirst("div#div_video iframe")?.attr("src") ?: return emptyList()
+            bloggerExtractor.videosFromUrl(url, headers)
         }
     }
 
