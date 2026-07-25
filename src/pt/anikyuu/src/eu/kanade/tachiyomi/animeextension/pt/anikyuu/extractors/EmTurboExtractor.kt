4a38fb59c232fe3031ea35acb3c215d5c7fb6015
Bashousen <109592830+Bashousen@users.noreply.github.com>
2026-07-25 20:29:52 -0300

    [pt/anikyuu] fix get code 429 when fetch segments - caused by wrong referer

--- src/pt/anikyuu/src/eu/kanade/tachiyomi/animeextension/pt/anikyuu/extractors/EmTurboExtractor.kt
+++ src/pt/anikyuu/src/eu/kanade/tachiyomi/animeextension/pt/anikyuu/extractors/EmTurboExtractor.kt
@@ -7,6 +7,7 @@ import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.coroutines.runBlocking
 import okhttp3.Headers
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 
@@ -25,7 +26,7 @@ class EmTurboExtractor(private val client: OkHttpClient, private val headers: He
 
         if (urlPlay.toHttpUrlOrNull() == null) return emptyList()
 
-        val videos = playlistExtractor.extractFromHls(urlPlay, url, videoNameGen = { quality -> "EmTurboVid - $quality" })
+        val videos = playlistExtractor.extractFromHls(urlPlay, "https://${url.toHttpUrl().host}/", videoNameGen = { quality -> "EmTurboVid - $quality" })
             .distinctBy { it.url } // they have the same stream repeated twice in the playlist file
 
         return runBlocking { m3u8Integration.processVideoList(videos) }
