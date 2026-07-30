1a95bd9613f9f1eca2e78f59466147280ac0c02e
Bashousen <109592830+Bashousen@users.noreply.github.com>
2026-07-29 21:45:10 -0300

    [pt/animeito] fix no available videos - add function to decode the script on AnimeItoExtractor

--- src/pt/animeito/src/eu/kanade/tachiyomi/animeextension/pt/animeito/extractors/AnimeItoExtractor.kt
+++ src/pt/animeito/src/eu/kanade/tachiyomi/animeextension/pt/animeito/extractors/AnimeItoExtractor.kt
@@ -3,9 +3,9 @@ package eu.kanade.tachiyomi.animeextension.pt.animeito.extractors
 import android.util.Base64
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
-import eu.kanade.tachiyomi.lib.unpacker.Unpacker
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import okhttp3.OkHttpClient
 
@@ -14,23 +14,19 @@ class AnimeItoExtractor(private val client: OkHttpClient, private val headers: H
 
     fun videosFromUrl(url: String): List<Video> {
         val playerDoc = client.newCall(GET(url, headers)).execute().asJsoup()
-        val encodedScript = playerDoc.selectFirst("script:containsData(AnimeiTo.Run)")
-            ?.data()
+        val encodedScript = playerDoc.selectFirst("[type=\"text/javascript\"]")
+            ?.data() ?: return emptyList()
 
-        val script = if (encodedScript != null) {
-            val decodedData = encodedScript.substringAfter("(").substringBefore(")")
-                .replace(Regex("\"\\s*\\+\\s*\""), "") // Remove concatenation
-                .replace(Regex("[^A-Za-z0-9+/=]"), "") // Remove non-base64 characters
-                .let { Base64.decode(it, Base64.DEFAULT) }
-                .let(::String)
-            Unpacker.unpack(decodedData).ifEmpty { return emptyList() }
-        } else {
-            playerDoc.selectFirst("script:containsData(const player)")?.data()
-                ?: return emptyList()
-        }
+        val arguments = REGEX_ARGUMENTS.find(encodedScript)?.groupValues ?: return emptyList()
 
-        return if ("googlevideo" in script) {
-            script.substringAfter("sources:").substringBefore("]")
+        val charDictionary: List<String> = Json.decodeFromString(arguments[1])
+        val charIndices: List<Int> = Json.decodeFromString(arguments[2])
+        val xorKeyBase64: String = arguments[3]
+
+        val decodedScript = decodeScript(charDictionary, charIndices, xorKeyBase64)
+
+        return if ("googlevideo" in decodedScript) {
+            decodedScript.substringAfter("sources\":").substringBefore("]")
                 .split("{")
                 .drop(1)
                 .map {
@@ -39,11 +35,32 @@ class AnimeItoExtractor(private val client: OkHttpClient, private val headers: H
                     Video(videoUrl, "Animei.to - $quality", videoUrl, headers)
                 }
         } else {
-            val masterPlaylistUrl = script.substringAfter("sources:")
+            val masterPlaylistUrl = decodedScript.substringAfter("sources\":")
                 .substringAfter("file\":\"")
                 .substringBefore('"')
 
             playlistUtils.extractFromHls(masterPlaylistUrl, videoNameGen = { "Animei.to - $it" })
         }
+    }
+
+    private fun decodeScript(charDictionary: List<String>, charIndices: List<Int>, xorKeyBase64: String): String {
+        val base64String = charIndices.joinToString("") { i -> charDictionary[i] }
+        val encodedPayload = String(Base64.decode(base64String, Base64.DEFAULT), Charsets.ISO_8859_1)
+        val xorKey = String(Base64.decode(xorKeyBase64, Base64.DEFAULT), Charsets.ISO_8859_1)
+
+        val decodedBytes = ByteArray(encodedPayload.length)
+        for (i in encodedPayload.indices) {
+            decodedBytes[i] = (
+                encodedPayload[i].code xor
+                    xorKey[i % xorKey.length].code
+                ).toByte()
+        }
+        val decodedScript = String(decodedBytes, Charsets.UTF_8)
+
+        return decodedScript
+    }
+
+    companion object {
+        val REGEX_ARGUMENTS = """\((\[[^]]*]),(\[[^]]*]),"([^"]*)""".toRegex()
     }
 }
